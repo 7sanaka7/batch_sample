b74a50523655e0f57067c07abbcbe88b37e7298b
okano sanaka <sanaka.7hex.bss@gmail.com>
2023-10-05 14:50:59 +0900

    getName変更

--- src/main/java/com/example/batchprocessing/Employee.java
+++ src/main/java/com/example/batchprocessing/Employee.java
@@ -18,7 +18,7 @@ public class Employee {
 	}
 	
 	//アクセサメソッド
-	public String getname() {
+	public String getName() {
 		return this.name;
 	}
 	
--- src/main/java/com/example/batchprocessing/EmployeeItemProcessor.java
+++ src/main/java/com/example/batchprocessing/EmployeeItemProcessor.java
@@ -2,7 +2,6 @@ package com.example.batchprocessing;
 
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
-import org.springframework.batch.item.ItemProcessor;
 
 public class EmployeeItemProcessor implements ItemProceccor<Employee, Employee> {
 	//ログ出力用のオブジェクトを生成
@@ -12,7 +11,7 @@ public class EmployeeItemProcessor implements ItemProceccor<Employee, Employee> 
 	@Override
 	public Employee process(final Employee employee) throws Exception{
 		//名前を大文字に
-		final String name = employee.getname().toUpperCase();
+		final String name = employee.getName().toUpperCase();
 		//フィールドを差し替えた新しいEmployeeオブジェクトを生成
 		final Employee transformedEmployee = new Employee(name, employee.getDepartment());
 		//差分をログに出力します
